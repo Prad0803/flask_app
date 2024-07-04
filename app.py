ddceb92374a82f228b12ed62c6aba604f2c8eda1
Prad0803 <sripradheep2003@gmail.com>
2024-07-04 17:27:12 +0530

    app.py

--- app.py
+++ app.py
@@ -72,4 +72,8 @@ def download_data():
     return send_file(data_path, as_attachment=True)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+        app.run(host='0.0.0.0', port=10000)
+    
+@app.route('/')
+def home():
+    return "Hello, Render!"
